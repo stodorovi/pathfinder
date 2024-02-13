092ac1490573dbb225505346039f0c7169975d34
sebastijan <todorovicsebastijan@gmail.com>
2024-02-13 17:12:30 +0100

    ui: toolbar set end node button

--- ui/components/toolbar/include/toolbar.h
+++ ui/components/toolbar/include/toolbar.h
@@ -14,6 +14,7 @@ private:
     algorithm_selection* _algorithm_selection;
     icon_button* _run_algorithm_btn;
     icon_button* _start_position_btn;
+    icon_button* _end_position_btn;
 
 public:
     explicit toolbar(QWidget* parent = nullptr);
